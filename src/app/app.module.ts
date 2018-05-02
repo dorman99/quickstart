295fc5a2622851f9ebdcf36cb2c88332621b421c
Vendor App <vendorapp@fadliHp-wireles.local>
2018-05-02 19:16:45 +0700

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { UserComponent } from './Components/User/User.component'
+import { UserComponent } from './Components/Tag/Tag.component'
 import { HobbiesComponent } from './Components/Hobbies/Hobbies.component'
 import { FormsModule } from '@angular/forms'
 
